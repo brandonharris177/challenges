0becbd076e44dc8e003cc896abe6ed0393786354
Brandon Harris <brandonharris177@gmail.com>
2021-08-05 15:43:34 -0600

    capitalize titles

--- challenges/views.py
+++ challenges/views.py
@@ -5,13 +5,13 @@ from django.template.loader import render_to_string
 
 # Create your views here.
 
-month_responces = {
-    "january" : "month of January",
-    "february" : "month of February"
+month_responses = {
+    "january" : "Challenge for January",
+    "february" : "Challenge for February"
 }
 
 def index(request):
-    months = list(month_responces.keys())
+    months = list(month_responses.keys())
     response = ""
     for month in months:
         path = reverse("string_month", args = [month])
@@ -22,12 +22,15 @@ def index(request):
 
 def monthly_challenge_by_number(request, month):
     try: 
-        return HttpResponseRedirect(reverse("string_month", args=[list(month_responces.keys())[month-1]]))
+        return HttpResponseRedirect(reverse("string_month", args=[list(month_responses.keys())[month-1]]))
     except:
         return HttpResponseNotFound("This month is not supported")
 
 def monthly_challenge(request, month):
     try:
-        return render(request, "challenges/challenge.html")
+        return render(request, "challenges/challenge.html", {
+            "month_name": month,
+            "text": month_responses[month]
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported<h1>")
